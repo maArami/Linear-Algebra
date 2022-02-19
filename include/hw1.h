bbfe16e803c1094da532104cd61fccbcb6732c5d
M.A.Arami <mohammad.arami1010@gmail.com>
2022-02-19 21:33:36 +0330

    make the zeros

--- include/hw1.h
+++ include/hw1.h
@@ -1,11 +1,14 @@
 #ifndef AP_HW1_H
 #define AP_HW1_H
+#include <iostream>
+#include <vector>
 
-namespace Algebra
+using Matrix = std::vector<std::vector<double>>;
+
+namespace algebra
 {
-
-
-    
+    Matrix zeros(size_t n, size_t m);
 }
+
 
 #endif //AP_HW1_H
--- src/hw1.cpp
+++ src/hw1.cpp
@@ -1,1 +1,17 @@
 #include "hw1.h"
+#include <iostream>
+#include <vector>
+
+using Matrix = std::vector<std::vector<double>>;
+
+Matrix algebra::zeros(size_t n, size_t m)
+{
+
+    Matrix mat(n , std::vector<double> (m,0));
+    
+    return mat;
+
+
+}
+
+
